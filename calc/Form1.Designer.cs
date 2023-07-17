5dea1695701c71e485bc56ab419e1fcfb55f0193
Vasvari Botond <89163562+Dejavu333@users.noreply.github.com>
2023-07-17 02:09:12 +0200

    Add files via upload

---
+++ calc/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.distance_textbox = new System.Windows.Forms.TextBox();
+            this.target_height_textbox = new System.Windows.Forms.TextBox();
+            this.current_height_textbox = new System.Windows.Forms.TextBox();
+            this.calc_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // distance_textbox
+            // 
+            this.distance_textbox.Location = new System.Drawing.Point(132, 51);
+            this.distance_textbox.Name = "distance_textbox";
+            this.distance_textbox.Size = new System.Drawing.Size(94, 27);
+            this.distance_textbox.TabIndex = 0;
+            // 
+            // target_height_textbox
+            // 
+            this.target_height_textbox.Location = new System.Drawing.Point(132, 84);
+            this.target_height_textbox.Name = "target_height_textbox";
+            this.target_height_textbox.Size = new System.Drawing.Size(94, 27);
+            this.target_height_textbox.TabIndex = 1;
+            // 
+            // current_height_textbox
+            // 
+            this.current_height_textbox.Location = new System.Drawing.Point(132, 117);
+            this.current_height_textbox.Name = "current_height_textbox";
+            this.current_height_textbox.Size = new System.Drawing.Size(94, 27);
+            this.current_height_textbox.TabIndex = 2;
+            // 
+            // calc_btn
+            // 
+            this.calc_btn.Location = new System.Drawing.Point(132, 150);
+            this.calc_btn.Name = "calc_btn";
+            this.calc_btn.Size = new System.Drawing.Size(94, 29);
+            this.calc_btn.TabIndex = 4;
+            this.calc_btn.Text = "calculate";
+            this.calc_btn.UseVisualStyleBackColor = true;
+            this.calc_btn.Click += new System.EventHandler(this.calc_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(232, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "m";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(232, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "m";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(232, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "m";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "target distance";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "target height";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 124);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "current height";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 255);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(192, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "velocity 100m/s   -->  angle";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 295);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(192, 20);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "velocity 160m/s   -->  angle";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 275);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(192, 20);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "velocity 130m/s   -->  angle";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(208, 255);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(18, 20);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "#";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(208, 275);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(18, 20);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "#";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(208, 295);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(18, 20);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "#";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(208, 315);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(18, 20);
+            this.label13.TabIndex = 18;
+            this.label13.Text = "#";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(12, 315);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(192, 20);
+            this.label14.TabIndex = 17;
+            this.label14.Text = "velocity 400m/s   -->  angle";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(150)))), ((int)(((byte)(127)))));
+            this.ClientSize = new System.Drawing.Size(539, 345);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calc_btn);
+            this.Controls.Add(this.current_height_textbox);
+            this.Controls.Add(this.target_height_textbox);
+            this.Controls.Add(this.distance_textbox);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "fire_pwner BOTY";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox distance_textbox;
+        private System.Windows.Forms.TextBox target_height_textbox;
+        private System.Windows.Forms.TextBox current_height_textbox;
+        private System.Windows.Forms.Button calc_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+    }
+}
---
+++ calc/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace calc
+{
+    public partial class Form1 : Form
+    {
+         public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calc_btn_Click(object sender, EventArgs e)
+        {
+            int[] velocities_nv = new int[] { 100, 130, 160, 400 };
+            string[] results_sv = new string[4];    //store possible angles for every velocity
+
+            /*--user input--*/
+            double distance_ns = Convert.ToDouble(distance_textbox.Text);
+            double currentHeight_ns = Convert.ToDouble(current_height_textbox.Text);
+            double targetHeight_ns = Convert.ToDouble(target_height_textbox.Text);
+
+            /*--calculate--*/
+            double heightDiff_ns = currentHeight_ns - targetHeight_ns;
+            bool isTargetHigher_bs = heightDiff_ns < 0 ? true : false;
+            heightDiff_ns = Math.Abs(heightDiff_ns);
+            double g = 9.8;
+
+            for (int i = 0; i < results_sv.Length; i++)
+            {
+            
+                double[] anglesRad_vn = new double[2];
+                double[] anglesDegree_vn = new double[2];
+
+                /*--same level--*/
+                if (heightDiff_ns == 0)
+                {
+                    //rad = (sin^-1(s*g/v²))/2          //if greater than 1, the target can't be reached no matter what the angle is
+                    anglesRad_vn[0] = Math.Asin(distance_ns * g / Math.Pow(velocities_nv[i], 2)) / 2;
+                    anglesRad_vn[1] = Math.PI / 2 - anglesRad_vn[0];
+                }
+                /*--lower level--*/
+                else if (heightDiff_ns != 0 && isTargetHigher_bs == false)
+                {
+                    double ß = g * Math.Pow(distance_ns, 2) / Math.Pow(velocities_nv[i], 2);
+                    double phi = Math.Atan(distance_ns / heightDiff_ns);
+                    double angle = (Math.Acos((ß - heightDiff_ns) / Math.Sqrt(Math.Pow(heightDiff_ns, 2) + Math.Pow(distance_ns, 2))) + phi) / 2;
+
+                    anglesRad_vn[0] = Math.Abs(angle);
+                    anglesRad_vn[1] = Math.PI / 2 - anglesRad_vn[0];
+                    //label15.Text = Convert.ToString(Math.Abs(angle));
+                }
+                /*--higher level--*/
+                //0=distance*tan(theta)-(g/2*t^2)tan^2(theta)-(g/2*t^2)
+                else if (heightDiff_ns != 0 && isTargetHigher_bs == true)
+                { 
+                    double t = distance_ns / velocities_nv[i];
+
+                    double a = g / 2 * Math.Pow(t, 2);
+                    double b = distance_ns * -1;
+                    double c = g / 2 * Math.Pow(t, 2) + heightDiff_ns;
+
+                    double sqrtPart_ns = Math.Pow(b, 2) - (4 * a * c);
+                    double root1_ns = ((-1) * b + Math.Sqrt(sqrtPart_ns)) / (2 * a);
+                    double root2_ns = ((-1) * b - Math.Sqrt(sqrtPart_ns)) / (2 * a);    //tan(theta) = root theta=?
+                            //label16.Text = $"{root1_ns} és {root2_ns}";     
+
+                    anglesRad_vn[0] = Math.Atan(root1_ns);
+                    anglesRad_vn[1] = Math.Atan(root2_ns);
+
+                            //label16.Text = $"{Math.Atan( root1_ns )*180/Math.PI} és {Math.Atan( root2_ns)*180/Math.PI}";
+                }
+
+                /*--output--*/
+                if (isTargetHigher_bs == false)
+                {
+                    if (anglesRad_vn[0] >= 0 && anglesRad_vn[0] <= 1)   //sin és cos max -1 1 lehet
+                    {
+                        anglesDegree_vn[0] = radToDeg(anglesRad_vn[0]);
+                        anglesDegree_vn[1] = radToDeg(anglesRad_vn[1]);
+
+                        results_sv[i] = $"{ toDegMinSecFormat(anglesDegree_vn[0]) } or { toDegMinSecFormat(anglesDegree_vn[1]) }";
+                    }
+                    else
+                    {
+                        results_sv[i] = "target can't be reached at this speed";
+                    }
+                }
+                else if (isTargetHigher_bs == true)
+                {
+                    anglesDegree_vn[0] = radToDeg(anglesRad_vn[0]);
+                    anglesDegree_vn[1] = radToDeg(anglesRad_vn[1]);
+                    
+                    if(anglesDegree_vn[0] <=0 || Double.IsNaN(anglesDegree_vn[0]) ) results_sv[i] = "target can't be reached at this speed"; //ha minusz fok jön ki
+                    else results_sv[i] = $"{ toDegMinSecFormat(anglesDegree_vn[0]) } or { toDegMinSecFormat(anglesDegree_vn[1]) }";
+                }
+            }
+
+            label10.Text = results_sv[0];
+            label11.Text = results_sv[1];
+            label12.Text = results_sv[2];
+            label13.Text = results_sv[3];
+        }//event ends
+
+        private string toDegMinSecFormat(double p_inDecimal_ns)
+        {
+            string result_ss;
+
+            int sec_ns = (int)Math.Round(p_inDecimal_ns * 3600);
+            int deg_ns = sec_ns / 3600;
+            sec_ns = Math.Abs(sec_ns % 3600);
+            int min_ns = sec_ns / 60;
+            sec_ns %= 60;
+            result_ss = $"{deg_ns}°{min_ns}'{sec_ns}''";
+            return result_ss;
+        }
+
+        private double radToDeg(double p_rad_ns)
+        {
+            return p_rad_ns * 180 / Math.PI;
+        }
+    }
+}
